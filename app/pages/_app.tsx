4b0ccced779f50b6f272507e5664766436704ac5
3pointdev <motors3996@gmail.com>
2023-12-04 10:30:07 +0900

    init

---
+++ app/pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { wrapper } from "src/redux/store";
+import "styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(pageProps);
+  return (
+    <Provider store={store}>
+      <Component {...props.pageProps} />
+    </Provider>
+  );
+}
---
+++ app/pages/index.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {}, []);
+
+  return (
+    <article className="text-bg-gray-900 bg-gray-300 dark:bg-gray-800 dark:text-white w-screen min-h-screen h-full"></article>
+  );
+}
